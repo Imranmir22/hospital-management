442a6fee42d8b133e4e09e74bf32a2ce3118a0d2
Imranmir22 <imranmir87543@gmail.com>
2026-07-26 10:25:22 +0530

    git ignore to ignore diist

--- dist/modules/patients/controllers/patients.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { PatientsService } from '../services/patients.service';
-import { CreatePatientDto } from '../dto/create-patient.dto';
-import { User } from '../../users/entities/user.entity';
-export declare class PatientsController {
-    private readonly patientsService;
-    constructor(patientsService: PatientsService);
-    upsert(createPatientDto: CreatePatientDto, user: User): Promise<import("../entities/patient.entity").Patient>;
-    getProfile(user: User): Promise<import("../entities/patient.entity").Patient>;
-}
